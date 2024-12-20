b433c52b35a57a38270fe0ba2c4a295f23275fe0
Will Ezell <will@dotcms.com>
2024-12-20 17:12:33 -0500

    removing optionality of NodeCache from the cacheService for linter

--- lib/cacheService.ts
+++ lib/cacheService.ts
@@ -1,12 +1,12 @@
 import NodeCache from 'node-cache';
 
 declare global {
-    var nodeCache: { instance?: NodeCache };
+    var nodeCache: { instance: NodeCache };
 }
 
 if (!global.nodeCache?.instance) {
     global.nodeCache = { instance: new NodeCache({ stdTTL: 3600, checkperiod: 120 }) };
 }
 
-export const dotCache = global.nodeCache.instance;
+export const dotCache: NodeCache = global.nodeCache.instance;
 
