9dbcb008ada48664884bd20d2f84033a34eb4d32
Jorge Conde <jorge@referralsaasquatch.com>
2025-01-20 10:20:29 -0800

    JC small css tweak

--- components/navigation/NavTree.js
+++ components/navigation/NavTree.js
@@ -6,7 +6,7 @@ const NavTree = React.memo(({ items, currentPath, level = 0 }) => {
 
   return (
     <div className="h-dvh">
-      <div className="space-y-2 min-w-64 pb-12">
+      <div className="space-t-2 min-w-64 pb-12">
         {items.map((item) => (
           <div key={item.title} className="mb-5">
             <div className="py-1 px-2 font-semibold">{item.title}</div>
