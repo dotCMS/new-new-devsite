8fac1cde8e5812a87f02acfa66bc82560f55072f
Will Ezell <will@dotcms.com>
2025-02-16 13:25:08 -0500

    fixing blog metadata

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -15,9 +15,61 @@ export async function generateMetadata({ params, searchParams }) {
     }
     const post = await getBlogDetailQuery(slug);
 
+    if (!post) {
+        return {
+            title: 'Blog Post Not Found',
+        };
+    }
+
+    const imageUrl = post.image?.fileAsset?.idPath 
+        ? `${post.host.hostname}/dA/${post.identifier}/70q/1000maxw/${post.inode}`
+        : `${post.host.hostname}/images/default-blog-image.jpg`;
+
     return {
         title: post.title,
+        description: post.teaser || `Read ${post.title} on dotCMS Developer Blog`,
         canonical: `${post.host.hostname}/blog/${post.urlTitle}`,
+        
+        // OpenGraph
+        openGraph: {
+            title: post.title,
+            description: post.teaser,
+            url: `${post.host.hostname}/blog/${post.urlTitle}`,
+            siteName: 'dotCMS Developer Blog',
+            images: [{
+                url: imageUrl,
+                width: 1200,
+                height: 630,
+                alt: post.image?.description || post.title,
+            }],
+            locale: 'en_US',
+            type: 'article',
+        },
+
+        // Twitter
+        twitter: {
+            card: 'summary_large_image',
+            title: post.title,
+            description: post.teaser,
+            images: [imageUrl],
+            creator: '@dotcms',
+            site: '@dotcms',
+        },
+
+        // Article specific
+        article: {
+            publishedTime: post.publishDate,
+            modifiedTime: post.modDate,
+            authors: post.author ? [`${post.author.firstName} ${post.author.lastName}`] : ['dotCMS Team'],
+            tags: post.tags,
+        },
+
+        // Other meta tags
+        keywords: post.tags?.join(', '),
+        robots: {
+            index: true,
+            follow: true,
+        },
     };
 }
 
@@ -49,4 +101,3 @@ export default async function BlogPage({ searchParams, params }) {
         </div>
     )
 }
-
