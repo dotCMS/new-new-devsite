40f2816f6a8f19aa5f2f5b5c529d9ccb0a028894
Will Ezell <will@dotcms.com>
2025-02-16 16:34:50 -0500

    adding blog seo, github cache purge action

--- util/utils.ts
+++ util/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function extractAssetId(uri: string) {
+    const match = uri.match(/\/dA\/([^/]+)/);
+    return match ? match[1] : null;
+}
