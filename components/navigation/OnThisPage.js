e861160692255b3179354ca81572f6dd34ef45d1
Jamie Mauro <jameson.mauro@dotcms.com>
2025-02-27 06:54:31 -0500

    fixes positioning for Newer/Older links on TOC

--- components/navigation/OnThisPage.js
+++ components/navigation/OnThisPage.js
@@ -60,14 +60,7 @@ export const OnThisPage = ({
   }
 
   return (
-    <div className="sticky top-8 pb-12 
-                overflow-y-auto p-4 px-2
-                [&::-webkit-scrollbar]:w-1.5
-                [&::-webkit-scrollbar-track]:bg-transparent
-                [&::-webkit-scrollbar-thumb]:bg-muted-foreground/10
-                [&::-webkit-scrollbar-thumb]:rounded-full
-                hover:[&::-webkit-scrollbar-thumb]:bg-muted-foreground/20
-                h-[calc(100vh-4rem)]" >
+    <div className="sticky top-8 mb-12" >
       {showOnThisPage && (
         <h3 className="mb-4 text-sm font-semibold">On This Page</h3>
       )}
