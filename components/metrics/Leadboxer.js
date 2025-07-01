79c6c8126ad71fafc2171721a8fcf0838eaed3a2
Will Ezell <will@dotcms.com>
2025-07-01 14:34:34 -0400

    installing leadboxer

--- components/metrics/Leadboxer.js
+++ components/metrics/Leadboxer.js
@@ -3,7 +3,7 @@ import Script from 'next/script';
 export function LeadboxerScript() {
     return (
         <Script
-            id="warmly-script-loader"
+            id="leadboxer-script"
             src="//script.leadboxer.com/?dataset=e554a68399035d2bebf4d6054a50b855"
             defer></Script>
     );
